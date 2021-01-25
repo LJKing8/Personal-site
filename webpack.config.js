4f8a9735f31e62d1119f817d0b29d404bd8d1ff1
LJKing8 <Laura.j.king8@gmail.com>
2021-01-25 10:37:52 -0800

    Setup Webpack

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+
+// webpack bundles data to help loading times and keeps everything orginized
+const currentTask = process.env.npm_lifecycle_event;
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse =require('fs-extra');
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('postcss-hexrgba'),
+    require('autoprefixer')
+];
+
+class RunAfterCompile {
+    apply(compiler){
+        compiler.hooks.done.tap('Copy images', function(){
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
+
+let cssConfig = {//test if the file name ends in .css
+    test: /\.css$/i,
+    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+};
+let pages = fse.readdirSync('./app').filter(function(file){
+    return file.endsWith('html')
+}).map(function(page){
+    return new HtmlWebpackPlugin({
+        filename: page,
+        template:`./app/${page}`
+    })
+})
+let config={
+    entry: './app/assets/scripts/App.js',// what we want to bundle
+    plugins: pages,
+    module: {
+        rules: [ //tells web pack what to do with specific files
+            cssConfig,
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
+if(currentTask == 'dev'){
+    cssConfig.use.unshift('style-loader');
+    config.output = {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    };// where we want to put the bundeled code
+    config.devServer ={
+        before: function(app, server) {
+            server._watch('./app/**/*.html');
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true, // allows web pack to insert new js and css without reload
+        port: 3000,
+        host: '0.0.0.0'//allows other devices on network to acess site
+    };
+
+    config.mode='development';
+
+}
+if(currentTask == 'build'){
+
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+    postCSSPlugins.push(require('cssnano'));
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    }
+    config.mode='production';
+    config.optimization={
+        splitChunks: {chunks: 'all'}
+    }
+    config.plugins.push(
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterCompile()
+    )
+}
+
+
+module.exports = config;
